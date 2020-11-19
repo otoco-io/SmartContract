0a98baeb5dc6ec9469275f9bbdfc9f23a199fac2
Filipe Soccol <filipe.soccol@gmail.com>
2020-11-19 10:26:36 -0300

    Finalizing tests.

--- migrations/2_previous_contracts.js
+++ migrations/2_previous_contracts.js
@@ -5,6 +5,7 @@ module.exports = async function (deployer, network, accounts) {
   if (network == 'main') return;
   await deployer.deploy(Token);
   let token = await Token.deployed();
+  await token.initialize('Dai test', 'DAI', 100000000, accounts[0]);
   await deployer.deploy(OtoCorp, token.address);
   let instance = await OtoCorp.deployed();
   console.log('Otoco Master', instance.address);
