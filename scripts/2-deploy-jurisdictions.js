08e3deacc846073155aed8aac7e34bc881a9c6c9
Filipe Soccol <filipe.soccol@gmail.com>
2024-02-22 16:58:10 -0300

    Deployed OtoCo to Base Sepolia Testnet  network.

--- scripts/2-deploy-jurisdictions.js
+++ scripts/2-deploy-jurisdictions.js
@@ -24,19 +24,19 @@ async function main() {
     try {
         settings = JSON.parse(fs.readFileSync(
             "./scripts/jurisdiction-settings.json",
-            {encoding:"utf-8"},
+            { encoding: "utf-8" },
         ));
     } catch (err) {
         console.log(
             `${FgRed}Error loading jurisdiction badges: ${err}${Reset}`
         );
-		process.exit(1);
+        process.exit(1);
     }
 
     try {
         deployed = JSON.parse(fs.readFileSync(
             `./deploys/v2/${network.name}.json`,
-            {encoding:"utf-8"},
+            { encoding: "utf-8" },
         ));
     } catch (err) {
         console.log(
@@ -45,11 +45,13 @@ async function main() {
     }
 
     switch (network.name) {
-      case "mainnet": settings = settings.mainnet; break;
-      case "polygon": settings = settings.polygon; break;
-      default: settings = settings.default; break;
+        case "mainnet": settings = settings.mainnet; break;
+        case "polygon": settings = settings.polygon; break;
+        case "base": settings = settings.base; break;
+        case "basesepolia": settings = settings.basesepolia; break;
+        default: settings = settings.default; break;
     }
-    
+
     /******************
      * USER PROMPTING *
      ******************/
@@ -57,14 +59,14 @@ async function main() {
     console.log(
         `\n${Bright}\t👤 Deploying contracts with ${FgCyan}${deployer.address}${Reset}`);
 
-    const deployerBalance = 
+    const deployerBalance =
         parseInt((await deployer.getBalance()).toString()) / 1e18;
-    
+
     console.log(
         `\t${Bright}💰 Balance: ${FgCyan}${deployerBalance.toFixed(4)} ETH${Reset}\n`);
 
     const explorer = networkId == '1' ? 'Etherscan' : networkId == '137' ? 'Polygonscan' : null;
-    
+
     if (explorer != null) {
         console.log(`${Bright} ${explorer} Gas Tracker Data: ${Reset}`);
         await hre.run("gas", { [network.name]: true });
@@ -74,42 +76,42 @@ async function main() {
      * ONCHAIN TASK *
      ****************/
 
-    const jurisdictions = 
-    await hre.run( "jurisdictions", { 
-        settings: JSON.stringify(settings),
-        predeployed: JSON.stringify(deployed.jurisdictions),
-        master: deployed.master
-    });
+    const jurisdictions =
+        await hre.run("jurisdictions", {
+            settings: JSON.stringify(settings),
+            predeployed: JSON.stringify(deployed.jurisdictions),
+            master: deployed.master
+        });
     deployed.jurisdictions = jurisdictions.map(({ address }) => address);
-    
-    
+
+
     /******************
      * STORAGE CHECKS *
      ******************/
 
     const jurisdictionData = {};
-    
+
     for (const jurisdiction of jurisdictions) {
-        const renewalPrice = 
+        const renewalPrice =
             (await jurisdiction.callStatic.getJurisdictionRenewalPrice()).toString();
-        const deployPrice = 
+        const deployPrice =
             (await jurisdiction.callStatic.getJurisdictionDeployPrice()).toString();
-        const closePrice = 
+        const closePrice =
             (await jurisdiction.callStatic.getJurisdictionClosePrice()).toString();
-        const name = 
+        const name =
             (await jurisdiction.callStatic.getJurisdictionName()).toString();
-        const defaultBadge = 
+        const defaultBadge =
             (await jurisdiction.callStatic.getJurisdictionBadge()).toString();
-        const goldBadge = 
+        const goldBadge =
             (await jurisdiction.callStatic.getJurisdictionGoldBadge()).toString();
-        jurisdictionData[jurisdiction.address] = 
+        jurisdictionData[jurisdiction.address] =
             { renewalPrice, deployPrice, closePrice, name, defaultBadge, goldBadge };
     }
 
     console.log(`${Bright}🚀 OtoCo V2 Jurisdictions Deployed:${Reset}`, jurisdictionData);
 
     fs.writeFileSync(
-        `./deploys/v2/${network.name}.json`, 
+        `./deploys/v2/${network.name}.json`,
         JSON.stringify(deployed, undefined, 2),
     );
 
@@ -120,18 +122,18 @@ async function main() {
 
     if (networkId != '31337') {
         for (const jurisdiction of jurisdictions) {
-            await hre.run( "verification", { 
+            await hre.run("verification", {
                 addr: jurisdiction.address,
                 args: JSON.stringify(
-                        Object.values(jurisdictionData[jurisdiction.address])),
+                    Object.values(jurisdictionData[jurisdiction.address])),
             });
         }
     }
 }
 
 main()
-.then(() => process.exit(0))
-.catch((error) => {
-    console.error(error);
-    process.exit(1);
-});
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
